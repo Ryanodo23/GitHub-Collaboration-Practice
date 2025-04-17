45a53af6a10c11b351067f849b08bd4f6f751516
odonnerl <odonnerl@LCB-01>
2025-04-17 12:03:23 -0400

    good method

--- GitHub Collaboration Practice/Student037.cs
+++ GitHub Collaboration Practice/Student037.cs
@@ -8,5 +8,9 @@ namespace GitHub_Collaboration_Practice
 {
     internal class Student037
     {
+        public static void Good()
+        {
+            Console.WriteLine("Hello World");
+        }
     }
 }
